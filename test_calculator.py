7ba350d73d47abc318552335e70f00241e8f52c3
Carl Lundholm <carl_0221@hotmail.se>
2026-01-29 10:48:43 +0100

    test: update test cases for add and divide methods

--- test_calculator.py
+++ test_calculator.py
@@ -8,10 +8,10 @@ class TestCalculator:
     
     '''Writing test cases for all methods in the Calculator class.'''
     def test_add(self):
-        assert self.calculator.add(2, 3) == 5
+        assert self.calculator.add(2, 5) == 7
     
     def test_divide(self):
-        assert self.calculator.divide(6, 3) == 2
+        assert self.calculator.divide(9, 3) == 3
     
     
     
